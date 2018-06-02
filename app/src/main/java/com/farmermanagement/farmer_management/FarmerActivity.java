53cb0555646b2e9d67c6e1930256de44e978ed28
Shruti Goyal <shruti.1109@gmail.com>
2018-06-02 10:23:03 +0530

    Add Farmer Screen Layout

---
+++ app/src/main/java/com/farmermanagement/farmer_management/FarmerActivity.java
@@ -0,0 +1,13 @@
+package com.farmermanagement.farmer_management;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FarmerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer);
+    }
+}
