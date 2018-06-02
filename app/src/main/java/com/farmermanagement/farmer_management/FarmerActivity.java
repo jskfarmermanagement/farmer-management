a0204696f1287d49700b851d9eb20017fe51a426
deepakkumarsinha <dsinha@servicesource.com>
2018-06-02 21:42:44 +0530

    remove un-used files

--- app/src/main/java/com/farmermanagement/farmer_management/FarmerActivity.java
+++ app/src/main/java/com/farmermanagement/farmer_management/FarmerActivity.java
@@ -1,7 +1,7 @@
 package com.farmermanagement.farmer_management;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class FarmerActivity extends AppCompatActivity {
 
